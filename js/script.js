ff8969b54e64a30b628c1bf81dc204108cfe0e0d
Gabriele Alessi <89archaon89@gmail.com>
2021-04-19 15:16:41 +0200

    mileston 2 completata ( aggiunta serie tv)

--- js/script.js
+++ js/script.js
@@ -4,11 +4,14 @@ function init() {
 
         data:{
             movies: [],
+            tvSeries:[],
             searched: ""
         },
 
         methods:{
             getApi: function () {
+
+                //film
                 axios.get('https://api.themoviedb.org/3/search/movie', {
                     params: {
                         'api_key': 'f1abffa0ec85176757c58a0ff27fccba',
@@ -20,10 +23,26 @@ function init() {
                     this.movies = moviesArray;
                 })
                 .catch(() => console.log('error'));
+
+
+                //serieTV
+                axios.get('https://api.themoviedb.org/3/search/tv', {
+                    params: {
+                        'api_key': 'f1abffa0ec85176757c58a0ff27fccba',
+                        'query': this.searched
+                    }
+                })
+                .then(data =>{
+                    const seriesArray = data.data.results;
+                    this.tvSeries = seriesArray;
+                })
+                .catch(() => console.log('error'));
             },
+
+            //mostra la bandiera se la nazionalità è En o It
             showFlag: function (language) {
                 if (language == "en" || language == "it" ) {
-                    return '<div class="row"><h3>Language: </h3><img src="img/' + language +'.png"></div>'
+                    return '<h3>Language: </h3><img src="img/' + language +'.png">'
                 }else {
                     return "<h3> Language: " + language +"</h3>"
                 }
